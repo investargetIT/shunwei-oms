12a221a12adbc8b7ba8187b8d8aa5efaf87ac828
Liu Hanjia <13099740@qq.com>
2024-08-21 17:21:45 +0800

    update code

--- src/main/java/com/yeebotech/shunweioms/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yeebotech.shunweioms.repository;
-
-import com.yeebotech.shunweioms.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
